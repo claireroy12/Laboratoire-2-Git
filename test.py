9c315bc57ba9b862050aeddac48c79e23446c069
claireroy12 <160762347+claireroy12@users.noreply.github.com>
2025-09-08 09:53:28 -0400

    Ajout de la nouvelle fonctionnalité
    Ajout de la nouvelle fonctionnalité (nombres d'essais)

--- test.py
+++ test.py
@@ -4,24 +4,29 @@ import random
 
 nombre = random.randint(0,100)
 boucle = True
+essais = 0
 
 while boucle == True:
 
     
     input1 = input("Devine mon chiffre : ")
+    essais +=1 
 
     try:
         if int(input1) == nombre :
             # Si le chiffre est le bon
             print("wow trop fort!")
             boucle = False
+            
         else :
             # Si le chiffre n'est pas le bon
             print("non, mais tu peux réessayer")
+            
     except:
         # permet de ne pas faire arrêter le programme
         print("un CHIFFRE plz")
         
+print("nombre d'essais :", essais)
 print("Bye !")
         
     
