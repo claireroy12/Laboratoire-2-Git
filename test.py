827e34939b2aadcc75063ac7d6b7627fe5629344
claireroy12 <160762347+claireroy12@users.noreply.github.com>
2025-09-08 09:50:09 -0400

    1.0
    Ajout de commentaires et commit final avant le push

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import random
 
+
+
 nombre = random.randint(0,100)
 boucle = True
 
@@ -10,11 +12,16 @@ while boucle == True:
 
     try:
         if int(input1) == nombre :
+            # Si le chiffre est le bon
             print("wow trop fort!")
             boucle = False
         else :
+            # Si le chiffre n'est pas le bon
             print("non, mais tu peux réessayer")
     except:
+        # permet de ne pas faire arrêter le programme
         print("un CHIFFRE plz")
+        
+print("Bye !")
         
     
